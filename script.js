6a44854a89619cd000dd98ca9120fd0605f7d760
larsz13 <lars.ziegler@zhdk.ch>
2022-08-18 16:08:55 +0200

    Basic animation added

--- script.js
+++ script.js
@@ -15,28 +15,32 @@ function initialisePage(mode){
   if(mode==0){ //EVENT
     linkToEvent.classList.toggle("hide");
     visionpage.classList.toggle("hide");
-    linkToVision.classList.toggle("animate");
     eventpage.classList.toggle("animate");
   }else if(mode==1){ //VISION
     linkToVision.classList.toggle("hide");
     eventpage.classList.toggle("hide");
-    linkToEvent.classList.toggle("animate");
     visionpage.classList.toggle("animate");
   }
 }
 
 // TOGGLE CLICK ON LINK CLICKED
 function toggleMode(){
+  reflow(0);
   linkToVision.classList.toggle("hide");
   eventpage.classList.toggle("hide");
   linkToEvent.classList.toggle("hide");
   visionpage.classList.toggle("hide");
-  linkToVision.classList.toggle("animate");
   eventpage.classList.toggle("animate");
-  linkToEvent.classList.toggle("animate");
   visionpage.classList.toggle("animate");
   if(mode==0){ //EVENT
     mode=1;
   }else if(mode==1){ //VISION
     mode=0;
   }}
+
+
+  // REFLOW FORCE
+
+function reflow(x){
+  console.log(x.offsetHeight);
+}
